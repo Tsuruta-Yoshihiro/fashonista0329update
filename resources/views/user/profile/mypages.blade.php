c3cc0ced4508cc9c6fa900b9733308277249c925
”Tsuruta_Yoshihiro” <ytsuruta720@gmail.com>
2020-04-10 14:15:34 +0000

    0410mypages_update

--- resources/views/user/profile/mypages.blade.php
+++ resources/views/user/profile/mypages.blade.php
@@ -71,7 +71,7 @@
                                     </div>
                                 </div>
                             <div id="content">
-                                <p class="btn">
+                                <p class="btnAdd">
                                     <a href="/user/upload/item/" class="over">コーディネートを投稿する</a>
                                 </p>
                             </div>
