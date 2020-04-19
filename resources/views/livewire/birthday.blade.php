f7ec29ba7e8dee8f02087ffb043d191fbca3a985
”Tsuruta_Yoshihiro” <ytsuruta720@gmail.com>
2020-04-19 14:24:44 +0000

    0419update

---
+++ resources/views/livewire/birthday.blade.php
@@ -0,0 +1,33 @@
+<div>
+    {{-- Care about people's approval and you will be their prisoner. --}}
+                            
+                            <!-- 年 -->
+                            <select name="birth_year" wire:model="year" wire:change="onChange">
+                                <option></option>
+                                @for($i = 1900 ; $i <= date('Y') ; $i++)
+                                <option value="{{ $i }}">{{ $i }}年</option>
+                                @endfor
+                            </select>
+                            
+                            <!-- 月 -->
+                            <select name="birth_month" wire:model="month" wire:change="onChange">
+                                <option></option>
+                                @for($i = 1 ; $i <= 12 ; $i++)
+                                <option value="{{ $i }}">{{ $i }}月</option>
+                                @endfor
+                            </select>
+                            
+                            <!-- 日 -->
+                            <select name="birth_day" wire:model="day" wire:change="onChange">
+                                <option></option>
+                                @for($i = 1 ; $i <= $last_day_of_month ; $i++)
+                                <option value="{{ $i }}">{{ $i }}日</option>
+                            　　@endfor
+                            </select>
+                            
+                            <!-- 年齢 -->
+                            @if($age > -1)
+                               $nbsp;/$nbsp;{{ $age }} 才
+                            @endif
+                            
+</div>
--- resources/views/user/profile/create.blade.php
+++ resources/views/user/profile/create.blade.php
@@ -6,6 +6,12 @@
 
 {{-- profile.blade.phpの@yield('content')に以下タグを埋め込む --}}
 @section('content')
+
+@livewireStyles
+@livewire('birthday')
+@livewireScripts
+
+
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
@@ -57,13 +63,33 @@
                         <div class="col-md-10">
                             
                             <!-- 年 -->
-                            
+                            <select name="birth-year" wire:model="year" wire:change="onChange">
+                                <option></option>
+                                @for($i = 1900 ; $i <= date('Y') ; $i++)
+                                <option value="{{ $i }}">{{ $i }}年</option>
+                                @endfor
+                            </select>
                             
                             <!-- 月 -->
-                            
+                            <select name="birth-month" wire:model="month" wire:change="onChange">
+                                <option></option>
+                                @for($i = 1 ; $i <= 12 ; $i++)
+                                <option value="{{ $i }}">{{ $i }}月</option>
+                                @endfor
+                            </select>
                             
                             <!-- 日 -->
+                            <select name="birth-day" wire:model="day" wire:change="onChange">
+                                <option></option>
+                                @for($i = 1 ; $i <= $last_day_of_month ; $i++)
+                                <option value="{{ $i }}">{{ $i }}日</option>
+                                @endfor
+                            </select>
                             
+                            <!-- 年齢 -->
+                            @if($age > -1)
+                                &nbsp;/&nbsp;{{ $age }} 才
+                            @endif
                             
                         </div>
                     </div>
